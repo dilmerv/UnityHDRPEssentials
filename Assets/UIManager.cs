caa4517026b9a5fdffea362661ed24f8bc3d3ad7
Dilmer Valecillos <dilmervst@gmail.com>
2019-03-25 21:51:53 -0600

    Added deformation UI for HDRP material

---
+++ Assets/UIManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private Slider timeScaleSlider;
+
+    [SerializeField]
+    private Text timeScaleSliderLabel;
+
+    [SerializeField]
+    private Slider noiseStegthScaleSlider;
+
+    [SerializeField]
+    private Text noiseStegthSliderLabel;
+
+    [SerializeField]
+    private Slider fresnelStegthScaleSlider;
+
+    [SerializeField]
+    private Text fresnelStegthSliderLabel;
+
+
+    [SerializeField]
+    private Material deformationMaterial;
+    
+    void Update()
+    {
+        deformationMaterial.SetFloat("_TimeSpeed", timeScaleSlider.value);    
+        timeScaleSliderLabel.text = $"Time Speed: {timeScaleSlider.value}";
+
+        deformationMaterial.SetFloat("_NoiseStrength", noiseStegthScaleSlider.value);    
+        noiseStegthSliderLabel.text = $"Noise Strength: {noiseStegthScaleSlider.value}";
+
+        deformationMaterial.SetFloat("_FresnelStrength", fresnelStegthScaleSlider.value);    
+        fresnelStegthSliderLabel.text = $"Fresnel Strength: {fresnelStegthScaleSlider.value}";
+    }
+}
